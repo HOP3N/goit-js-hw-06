4463cca0d4ecb5c9d5caae1cf7aef39dffd37a35
HOP3N <pankoff97@gmail.com>
2023-01-17 20:20:34 +0200

    t3 still?? +++ t4+

---
+++ src/js/task-04.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let valueEl = Number(document.querySelector("#value").textContent);
+
+let counterValue = document.querySelector("#value");
+
+const decrementBtn = document.querySelector("#counter").firstElementChild;
+const incrementBtn = document.querySelector("#counter").firstElementChild;
+
+incrementBtn.addEventListener("click", event => {
+    valueEl += 1;
+
+    counterValue.innerText = valueEl;
+});
+
+decrementBtn.addEventListener("click", event => {
+    valueEl -= 1;
+
+    counterValue.innerText = valueEl;
+});
