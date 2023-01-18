6f5c59236b3c52c21f47c00825903d632e4735dc
HOP3N <pankoff97@gmail.com>
2023-01-18 14:54:57 +0200

    t9+

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtnEl = document.querySelector("button.change-color");
+const bodyEl = document.querySelector("body");
+const spanColorEl = document.querySelector("span.color");
+
+changeBtnEl.addEventListener("click", (e) => {
+  let colorEl = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorEl;
+  spanColorEl.textContent = colorEl;
+});
